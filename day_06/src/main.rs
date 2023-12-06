590555c1429cd542e0e717868d3f20a05d0ec06d
Giacomo Pilotti <jack.pilo04@gmail.com>
2023-12-06 13:17:40 +0100

    Day 6

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -2,6 +2,7 @@ use std::{
     env,
     fs::File,
     io::{BufRead, BufReader, Error},
+    iter::zip,
 };
 
 fn main() -> Result<(), Error> {
@@ -10,6 +11,8 @@ fn main() -> Result<(), Error> {
     let input_file = File::open(input_filepath)?;
     let input_file_buf = BufReader::new(input_file);
 
+    let mut times = vec![];
+    let mut distances = vec![];
     let mut time = 0;
     let mut distance = 0;
     for (ix, line) in input_file_buf
@@ -17,31 +20,47 @@ fn main() -> Result<(), Error> {
         .enumerate()
         .map(|(ix, line)| (ix, line.expect("Expecting a line here")))
     {
-        let number = line.split(':').collect::<Vec<&str>>()[1]
+        let line = line.split(':').collect::<Vec<&str>>()[1];
+        let numbers = line
+            .split_ascii_whitespace()
+            .map(|n| n.parse::<u64>().expect("Expecting a number"))
+            .collect::<Vec<u64>>();
+        let number = line
             .replace(" ", "")
             .parse::<u64>()
             .expect("Expecting a number");
 
         if ix == 0 {
+            times = numbers;
             time = number;
         } else {
+            distances = numbers;
             distance = number;
         }
     }
 
-    let mut result = None;
-    let exact = if time % 2 == 0 { true } else { false };
+    let result_0 = foo(&times, &distances);
+    let result_1 = foo(&[time], &[distance]);
 
-    let mut wins: u64 = 0;
-    for t in 1..=(time / 2) {
-        let r = t * (time - t);
-        if r > distance {
-            wins += if exact && t == time / 2 { 1 } else { 2 };
-        }
-    }
-    result = Some(result.unwrap_or(1) * wins);
-
-    println!("{}", result.unwrap_or_default());
+    println!("{}", result_0);
+    println!("{}", result_1);
 
     Ok(())
+}
+
+fn foo(times: &[u64], distances: &[u64]) -> u64 {
+    let mut result = None;
+    for (time, distance) in zip(times, distances) {
+        let exact = if time % 2 == 0 { true } else { false };
+        let mut wins: u64 = 0;
+        for t in 1..=(time / 2) {
+            let r = t * (time - t);
+            if r > *distance {
+                wins += if exact && t == time / 2 { 1 } else { 2 };
+            }
+        }
+        result = Some(result.unwrap_or(1) * wins);
+    }
+
+    result.unwrap_or_default()
 }
