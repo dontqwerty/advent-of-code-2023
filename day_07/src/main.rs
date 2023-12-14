83458cd91707a142d01901d8f01452c2cdbdf329
Giacomo Pilotti <jack.pilo04@gmail.com>
2023-12-14 17:17:01 +0100

    Part 2

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -6,101 +6,109 @@ use std::{
     iter::zip,
 };
 
-fn main() -> Result<(), Error> {
-    let args: Vec<String> = env::args().collect();
-    let input_filepath = args.get(1).expect("Please specify the input filepath");
-    let input_file = File::open(input_filepath)?;
-    let input_file_buf = BufReader::new(input_file);
+static CARD_MAP_0: &str = "AKQJT98765432";
+static CARD_MAP_1: &str = "AKQT98765432J";
 
-    let mut hands = vec![];
-    let mut bids = vec![];
-    for line in input_file_buf
-        .lines()
-        .map(|line| line.expect("Expecting a line here"))
-    {
-        let line = line
-            .split(' ')
-            .map(|s| s.to_string())
-            .collect::<Vec<String>>();
-        hands.push(line[0].to_owned());
-        bids.push(line[1].to_owned());
-    }
+#[derive(Debug)]
+struct Hand {
+    cards: Vec<usize>,
+    bid: usize,
+    strength: usize,
+}
 
-    let mut results: Vec<(Vec<usize>, usize, usize)> = vec![];
-    for (hand, bid) in zip(&hands, &bids) {
-        let mut card_count: HashMap<char, usize> = HashMap::new();
-        for card in hand.chars() {
-            if let Some(prev_count) = card_count.insert(card, 1) {
-                let current_card_count = card_count
-                    .get_mut(&card)
-                    .expect("Expected value with this key");
-                *current_card_count += prev_count;
+impl Hand {
+    fn from_line(line: &str, with_jokers: bool) -> Self {
+        let cards_and_bid = line.split_ascii_whitespace().collect::<Vec<&str>>();
+
+        let card_map = if with_jokers { CARD_MAP_1 } else { CARD_MAP_0 };
+
+        let cards = cards_and_bid[0]
+            .chars()
+            .map(|c| card_map.find(c).unwrap())
+            .collect::<Vec<usize>>();
+        let bid = cards_and_bid[1].parse::<usize>().unwrap();
+
+        let mut card_counts = HashMap::new();
+        for card in cards.iter() {
+            if card_counts.get(card).is_some() {
+                continue;
             }
+            let count = cards.iter().filter(|c| *c == card).count();
+            card_counts.insert(card, count);
         }
 
-        let hand_type: usize = if card_count.values().any(|count| count == &5) {
+        let counts = if with_jokers {
+            let jokers_count = *card_counts.get(&12).unwrap_or(&0);
+            if jokers_count == 5 {
+                vec![5]
+            } else {
+                let mut counts = card_counts
+                    .iter()
+                    .filter(|cc| **cc.0 != 12)
+                    .map(|cc| *cc.1)
+                    .collect::<Vec<usize>>();
+                *counts.iter_mut().max().unwrap() += jokers_count;
+
+                counts
+            }
+        } else {
+            card_counts.iter().map(|cc| *cc.1).collect::<Vec<usize>>()
+        };
+
+        let strength = if counts.iter().any(|c| *c == 5) {
             0
-        } else if card_count.values().any(|count| count == &4) {
+        } else if counts.iter().any(|c| *c == 4) {
             1
-        } else if card_count.values().any(|count| count == &3)
-            && card_count.values().any(|count| count == &2)
-        {
+        } else if counts.iter().any(|c| *c == 3) && counts.iter().any(|c| *c == 2) {
             2
-        } else if card_count.values().any(|count| count == &3) {
+        } else if counts.iter().any(|c| *c == 3) {
             3
-        } else if card_count.values().filter(|count| count == &&2).count() == 2 {
+        } else if counts.iter().filter(|c| **c == 2).count() == 2 {
             4
-        } else if card_count.values().any(|count| count == &2) {
+        } else if counts.iter().any(|c| *c == 2) {
             5
         } else {
             6
         };
 
-        let values = String::from("AKQJT98765432");
-        results.push((
-            hand.chars()
-                .map(|c| values.find(c).unwrap())
-                .collect::<Vec<usize>>(),
-            bid.parse::<usize>().expect("Expected bid to be a number"),
-            hand_type,
-        ));
-    }
-
-    // type: [(hand, bid)]
-    let mut same_types: HashMap<usize, Vec<(&Vec<usize>, usize)>> = HashMap::new();
-    for ix in 0..results.len() {
-        if same_types.contains_key(&results[ix].2) {
-            continue;
-        }
-
-        let mut same_type = results
-            .iter()
-            .filter(|r| r.2 == results[ix].2)
-            .map(|r| (&r.0, r.1))
-            .collect::<Vec<(&Vec<usize>, usize)>>();
-
-        same_type.sort_by_key(|s| s.0);
-        same_types.insert(results[ix].2, same_type);
-    }
-
-    // type: [(hand, bid)]
-    let mut same_types: Vec<(&usize, &Vec<(&Vec<usize>, usize)>)> =
-        Vec::from_iter(same_types.iter());
-    same_types.sort_by_key(|c| c.0);
-
-    let mut results = vec![];
-    for v in same_types.iter() {
-        for vv in v.1.iter() {
-            results.push(vv.1);
+        Self {
+            cards,
+            bid,
+            strength,
         }
     }
-    let mut total = 0;
+}
 
-    for (i, r) in results.iter().enumerate() {
-        total += (results.len() - i) * r;
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+    let input_filepath = args.get(1).expect("Please specify the input filepath");
+    let input_file = File::open(input_filepath)?;
+
+    let input_file_buf = BufReader::new(input_file);
+
+    let mut hands_0 = vec![];
+    let mut hands_1 = vec![];
+    for line in input_file_buf
+        .lines()
+        .map(|line| line.expect("Expecting a line here"))
+    {
+        hands_0.push(Hand::from_line(&line, false));
+        hands_1.push(Hand::from_line(&line, true));
     }
 
-    println!("{}", total);
+    hands_0.sort_by_key(|h| (h.strength, h.cards.clone()));
+    hands_1.sort_by_key(|h| (h.strength, h.cards.clone()));
+
+    let hands_count = hands_0.len();
+    let mut total_0 = 0;
+    let mut total_1 = 0;
+    for (ix, (hand_0, hand_1)) in zip(hands_0, hands_1).enumerate() {
+        total_0 += hand_0.bid * (hands_count - ix);
+        total_1 += hand_1.bid * (hands_count - ix);
+    }
+
+    println!("{}", total_0);
+    println!("{}", total_1);
 
     Ok(())
 }
